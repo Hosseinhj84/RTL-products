ffada8e53970d584787539de11ef9a0b617f90cc
Hosseinhj84 <125790298+Hosseinhj84@users.noreply.github.com>
2025-11-25 18:41:13 +0000

    refactor: remove AboutMe and Intro components from App

--- demos/demo1/resume-template/src/App.jsx
+++ demos/demo1/resume-template/src/App.jsx
@@ -5,14 +5,7 @@ import Intro from './components/Intro'
 export default function App() {
 
   return (
-        <div className="w-full min-h-screen bg-gray-50 text-right direction-rtl">
-      <Intro role="front-end Developer" img="#"/>
-      <AboutMe
-        age="21"
-        university="دانشگاه XYZ"
-        careerGoal="Front-end Developer"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ullam fugit error inventore expedita illum accusantium perferendis, a consectetur dolore corporis possimus, nulla, obcaecati quaerat."
-      />
+    <div className="App">
     </div>
   )
 }
